e7cac192bde44778e1827b5c4f42b0904dc15e46
Galyna <galynag.work@gmail.com>
2017-05-19 05:40:56 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    $('.close').click(function () {
+        $('.search_box input').val('');
+        $('.search_box input').focus();
+    });
+    $('.details').click(function () {
+        $( ".user_info .info_user_detail" ).slideToggle( "slow", function() {
+        });
+        $(".user_info .left_side i").toggleClass("fa-angle-up fa-angle-down");
+
+    });
+    $('.mob_filter.sort').click(function () {
+        console.log("fs");
+        $(".right_menu.image").toggleClass("mob_view");
+        $(".images_page .mob_filter.sort .filter i").toggleClass("fa-angle-up fa-angle-down");
+
+
+    });
+});
